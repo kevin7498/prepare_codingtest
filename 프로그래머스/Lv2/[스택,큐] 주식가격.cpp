2c1af4c8157d8816025994b6e89f3d87ca41fbc3
kevin7498 <61459690+kevin7498@users.noreply.github.com>
2021-06-29 16:21:21 +0900

    Update [스택,큐] 주식가격.cpp

--- 프로그래머스/Lv2/[스택,큐] 주식가격.cpp
+++ 프로그래머스/Lv2/[스택,큐] 주식가격.cpp
@@ -7,7 +7,6 @@ int* solution(int prices[], size_t prices_len) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     int* answer = (int*)malloc(sizeof(int) * prices_len);
     int count = 0;
-    int temp = 0;
     
     for(int i = 0; i < prices_len - 1; i++){
         for(int j = i + 1; j < prices_len; j++){
