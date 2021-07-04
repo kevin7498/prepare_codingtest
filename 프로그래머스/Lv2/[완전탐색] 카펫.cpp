8f6001cfc6ffc9ed957abb2bd0d5f56adcc64ae2
kevin7498 <61459690+kevin7498@users.noreply.github.com>
2021-07-04 13:26:06 +0900

    Create [완전탐색] 카펫.cpp

---
+++ 프로그래머스/Lv2/[완전탐색] 카펫.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow) {
+    vector<int> answer;
+    vector<pair<int, int>> find;
+
+    int total = brown + yellow;
+    for(int i = 1; i * i <= total; i++){
+        if(total % i == 0){
+            find.push_back(make_pair(i, total / i));
+        }
+    }
+
+    for(int i = 0; i < find.size(); i++){
+        int h = find[i].first;
+        int w = find[i].second;
+        if(h - 2 > 0 && w - 2 > 0){
+            if((h - 2)*(w - 2) == yellow){
+                answer.push_back(w);
+                answer.push_back(h);
+            } 
+        }
+    }    
+    return answer;
+}
