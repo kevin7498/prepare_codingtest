a518eb54e34b5edb620aec58c10aa67204684687
kevin7498 <61459690+kevin7498@users.noreply.github.com>
2021-08-18 12:45:27 +0900

    Create 오픈채팅방.cpp

---
+++ 프로그래머스/Lv2/오픈채팅방.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <sstream>
+
+using namespace std;
+
+vector<string> solution(vector<string> record) {
+    vector<string> answer;    
+    map<string, string> usid;
+    map<string, string>::iterator iter;
+    
+    //map에 아이디 + 마지막 닉네임 뽑아서
+    for(int i = record.size() - 1; i >= 0; i--){
+        string text = record[i];
+        istringstream ss(text);
+        string id, name;
+        getline(ss, id, ' ');
+        getline(ss, id, ' ');
+        getline(ss, name, ' ');
+        if(name != "")
+            usid.insert(pair<string, string>(id, name));
+    }
+    
+    for(int i = 0; i < record.size(); i++){
+        string text = record[i];
+        istringstream ss(text);
+        string command, id;
+        getline(ss, command, ' ');
+        getline(ss, id, ' ');
+               
+        if(command == "Enter")
+            answer.push_back(usid[id] + "님이 들어왔습니다.");
+        else if(command == "Leave")
+            answer.push_back(usid[id] + "님이 나갔습니다.");
+    }
+    return answer;
+}
